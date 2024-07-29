bd6cd9ba5d77160f8267f4c1494b7f35706b4d8c
hassanosamaa <almsryh877@gmail.com>
2024-07-30 02:07:56 +0300

    empty project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 
 
-import Home from "../src/pages/home/Home";
-import Details from "./pages/Details/Details";
-import Layout from "./Layout";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
-let r=createBrowserRouter([
-  {path:"",element:<Layout/>,children:[
-    {index:true,element:<Home/>},
-    {path:":type",element:<Home/>},
-    {path:"latest/:type",element:<Home/>},
-    {path:":type/:title",element:<Details/>},
-    {path:"latest/:type/:title",element:<Details/>},
-  ]}
-])
+
 function App() {
 
   return (
     <>
-     <RouterProvider router={r}></RouterProvider>
     </>
   )
 }
